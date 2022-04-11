0e0f5b01cb5c742b019b3c42c6ccf4bbbb11708c
WiIIiam <us.thanhlong18@gmail.com>
2022-04-11 11:27:36 +0700

    login, logout, forgot email, CRUD product

---
+++ backend/Controller/UserController.js
@@ -0,0 +1,152 @@
+const User = require("../models/UserModel");
+const ErrorHandler = require("../utils/ErrorHandler.js");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const Features = require("../utils/Features");
+const sendMail = require("../utils/sendMail.js");
+const sendToken = require("../utils/jwtToken");
+
+// Register user
+exports.createUser = catchAsyncErrors(async (req, res, next) => {
+  //   const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+  //     folder: "avatars",
+  //     width: 150,
+  //     crop: "scale",
+  //   });
+
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "https://test.com", //myCloud.public_id
+      url: "https://test.com", //myCloud.secure_url
+    },
+  });
+
+  sendToken(user, 200, res);
+});
+
+// Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter the email & password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(
+      new ErrorHandler("User is not find with this email & password", 401)
+    );
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(
+      new ErrorHandler("User is not find with this email & password", 401)
+    );
+  }
+  const token = user.getJwtToken();
+
+  sendToken(user, 201, res);
+});
+
+//  Log out user
+exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Log out success",
+  });
+});
+
+// Forgot password
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorHandler("User not found with this email", 404));
+  }
+
+  // Get ResetPassword Token
+
+  const resetToken = user.getResetToken();
+
+  await user.save({
+    validateBeforeSave: false,
+  });
+
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/password/reset/${resetToken}`;
+
+  const message = `Your password reset token is :- \n\n ${resetPasswordUrl}`;
+
+  try {
+    await sendMail({
+      email: user.email,
+      subject: `iMusical Password Recovery`,
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} succesfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordTime = undefined;
+
+    await user.save({
+      validateBeforeSave: false,
+    });
+
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+
+
+// Reset Password
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+    // Create Token hash
+  
+    const resetPasswordToken = crypto
+      .createHash("sha256")
+      .update(req.params.token)
+      .digest("hex");
+  
+    const user = await User.findOne({
+      resetPasswordToken,
+      resetPasswordTime: { $gt: Date.now() },
+    });
+  
+    if (!user) {
+      return next(
+        new ErrorHandler("Reset password url is invalid or has been expired", 400)
+      );
+    }
+  
+    if (req.body.password !== req.body.confirmPassword) {
+      return next(
+        new ErrorHandler("Password is not matched with the new password", 400)
+      );
+    }
+  
+    user.password = req.body.password;
+  
+    user.resetPasswordToken = undefined;
+    user.resetPasswordTime = undefined;
+  
+    await user.save();
+  
+    sendToken(user, 200, res);
+  });
--- backend/app.js
+++ backend/app.js
@@ -8,30 +8,29 @@ const dotenv = require("dotenv");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(fileUpload());
 
 // config
 dotenv.config({
-    path:"backend/config/.env"
-})
+  path: "backend/config/.env",
+});
 
 // Route imports
 const product = require("./Routes/ProductRoute");
-// const user = require("./Routes/UserRoute");
+const user = require("./Routes/UserRoute");
 // const order = require("./Routes/OrderRoute");
 // const payment = require("./Routes/PaymentRoute");
 
-app.use("/api/v1",product);
+app.use("/api/v1", product);
 
-// app.use("/api/v1",user);
+app.use("/api/v1", user);
 
 // app.use("/api/v1",order);
 
 // app.use("/api/v1",payment);
 
-
 // it's for errorHandeling
 app.use(ErrorHandler);
 
-module.exports = app
+module.exports = app;
